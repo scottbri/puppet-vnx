bb3a0c8eae7c9e3569b6e006b65852277559a3fc
@scottbri <scott@brightwell.org>
2015-12-08 15:01:42 -0500

    added demo vnx.pp manifest

---
+++ vnx.pp
@@ -0,0 +1,41 @@
+transport {'vnx5400':
+  username => 'sysadmin',
+  password => 'sysadmin',
+  server   => '192.168.1.31',
+  cli_path => '"C:\Program Files (x86)\EMC\Navisphere CLI\naviseccli.exe"'
+}
+
+vnx_storagepool {"puppet-pool":
+  transport => Transport['vnx5400'],
+  disks => ['0_0_9', '0_0_10', '0_0_11', '0_0_12', '0_0_13', '1_1_9', '1_1_10', '1_1_11', '1_1_12', '1_1_13'],
+  raid_type => 'r_5',
+  auto_tiering => 'scheduled',
+  ensure_fastcache => 'true',
+  ensure => present
+}
+
+vnx_lun {"sds01":
+  transport => Transport['vnx5400'],
+  require => Vnx_storagepool['puppet-pool'],
+  pool_name => 'puppet-pool',
+  type => 'thin',
+  capacity => '10',
+  size_qual => 'gb',
+  initial_tier => 'highest_available',
+  tiering_policy => 'auto_tier',
+  auto_assign => 'true',
+  ensure => present
+}
+
+vnx_lun {"sds02":
+  transport => Transport['vnx5400'],
+  require => Vnx_storagepool['puppet-pool'],
+  pool_name => 'puppet-pool',
+  type => 'thin',
+  capacity => '10',
+  size_qual => 'gb',
+  initial_tier => 'highest_available',
+  tiering_policy => 'auto_tier',
+  auto_assign => 'true',
+  ensure => present
+}
